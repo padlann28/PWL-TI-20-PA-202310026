615cc9e366a82ed5a052eda8ddf98aa0c3d6d5c8
unknown <202310026@student.ibik.ac.id>
2023-03-24 16:19:30 +0700

    PWL-TI-20-PA-202310026

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+           <h1>hello</h1>
+           <p>lorem ipsum dolor sit amet</p>
+
+           <h1>Call function dgn expression</h1>
+      </div>
+    )
+  }
+}
